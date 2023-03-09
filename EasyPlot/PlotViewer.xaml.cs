399699ce915a4971144ffa59a4093e067fa5d929
Yashwanth Reddy Yarala <yashwanthreddy.yarala@tmeic.in>
2023-03-10 01:12:34 +0530

    PlotViewer Added // Its a window populated when called by passing a pot or ui element of list of plots as argument displaying the Plots as an output . //

---
+++ EasyPlot/PlotViewer.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EasyPlot
+{
+    /// <summary>
+    /// Interaction logic for PlotViewer.xaml
+    /// Pass the wpf plot as a parameter and PlotViewer.Show() populates the graph in a new window
+    /// </summary>
+    public partial class PlotViewer : Window
+    {
+        public PlotViewer(WpfPlot plot)
+        {
+            InitializeComponent();
+            mainwindow_grid.Children.Add(plot);
+        }
+        public PlotViewer(UIElement uIElement)
+        {
+            mainwindow_grid.Children.Add((UIElement)uIElement);
+        }
+    }
+}
